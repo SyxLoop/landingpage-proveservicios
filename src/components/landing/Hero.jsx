5ba350a595757c81621a8f73a5510cc5e2987779
Diego Quintana <145235994+SyxLoop@users.noreply.github.com>
2026-01-03 21:54:58 -0500

    Revert "Merge branch 'master' into development"
    This reverts commit d8ab1749060163c32c7944a8a4408e246b3f9904, reversing
    changes made to ff26690c106d2e863d69aadb513d4d3336d153b3.

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -14,15 +14,11 @@ export default function Hero() {
       <div className="relative flex flex-col h-full w-full place-items-center place-content-center gap-8">
         <h1 className="text-white text-5xl md:text-6xl text-center leading-tight">
           Proveedora de Servicios <br />
-          <span className="text-white text-3xl md:text-5xl">
-            para Automotores S.A
-          </span>
+          <span className="text-white text-3xl md:text-5xl">para Automotores S.A</span>
         </h1>
         <h2 className="text-white text-lg md:text-2xl">
           Trabajo y servicio de la mejor{" "}
-          <span className="text-accent text-2xl md:text-3xl font-bold">
-            Calidad
-          </span>
+          <span className="text-accent text-2xl md:text-3xl font-bold">Calidad</span>
         </h2>
         <Button
           text={"Contáctanos"}
--- src/components/landing/Services.jsx
+++ src/components/landing/Services.jsx
@@ -1,13 +1,64 @@
+import Card from "@components/globals/Card";
+
+const servicesData = [
+  {
+    id: 1,
+    icon: "",
+    title: "Venta de Combustible",
+    description: "Alta calidad para su vehículo",
+  },
+  {
+    id: 2,
+    icon: "",
+    title: "Atención al Cliente",
+    description: "Atención con personal calificado y comprometido",
+  },
+  {
+    id: 3,
+    icon: "",
+    title: "Aditivos y Lubricantes para Automóviles",
+    description: "Suministro de aditivos y lubricantes para automóviles",
+  },
+  {
+    id: 4,
+    icon: "",
+    title: "Recibos Electronicos",
+    description: "Contamos con tecnología INNOVA",
+  },
+  {
+    id: 5,
+    icon: "",
+    title: "Puntos Colombia",
+    description: "Reclama y redime tus puntos Colombia",
+  },
+];
+
 export default function Services() {
   return (
-    <section className="w-full h-dvh bg-accent/10">
+    <section className="w-full h-full py-12 md:py-0 md:h-dvh bg-accent/25">
       <div className="flex flex-col h-full place-items-center place-content-center">
         <div className="flex flex-col place-items-center place-content-center mb-14 gap-8 font-bold">
-          <h2 className="text-3xl md:text-5xl text-surface mb-12">Nuestros Servicios</h2>
+          <h2 className="text-3xl md:text-5xl text-surface mb-12">
+            Nuestros Servicios
+          </h2>
           <p className="w-3/4 text-center text-lg  md:text-[20px] whitespace-normal text-gray font-medium">
             Aceptamos todas las tarjetas de crédito y débito, incluyendo Visa y
             Mastercard.
           </p>
+        </div>
+        <div className="grid w-4/5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-auto">
+          {servicesData.map((service) => (
+            <Card>
+              <Card.Body>
+                <Card.Title className="text-surface">
+                  {service.title}
+                </Card.Title>
+                <Card.Text className="text-gray">
+                  {service.description}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          ))}
         </div>
       </div>
     </section>
